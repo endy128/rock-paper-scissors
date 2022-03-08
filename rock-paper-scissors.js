3fc9f780a60dd6d3cdff5a07bdb1af26dd113563
Neil Douglas <endy64@icloud.com>
2022-03-08 19:24:08 +0000

    added buttons, used DOM manipulation for results

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -8,90 +8,102 @@ function computerPlay() {
     random = Math.floor(random);
 
     // initialise computerSelection variable, set it to ""
-    let computerSelection = "";
+    let computerSelection = '';
 
     // if "random" = 1, computerSelection = "Rock"
     if (random == 1) {
-        computerSelection = "Rock";
+        computerSelection = 'rock';
     } else if (random == 2) {
     // if "random" = 2, computerSelection = "Paper"
-        computerSelection = "Paper";
+        computerSelection = 'paper';
     } else {
         // if "random" = 3, computerSelection = "Scissors"
-        computerSelection = "Scissors";
+        computerSelection = 'scissors';
     }
 // return computerselection
 return(computerSelection);
 }
 
-
+// plays round of rock, paper scissors and returns the result
 function playRound(playerSelection, computerSelection) {
-    let result = "";
+    let result = '';
 
-    if (playerSelection == "Paper" && computerSelection == "Rock") {
-        result = "P"; // Player wins
+    if (playerSelection == 'paper' && computerSelection == 'rock') {
+        result = 'playerWins'; // Player wins
         return(result);
-    } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
-        result = "P"; // Player wins
+    } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        result = 'playerWins'; // Player wins
         return(result);
-    } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
-        result = "P"; // Player wins
+    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        result = 'playerWins'; // Player wins
         return(result);
     } else if (playerSelection == computerSelection) {
-        result = "D"; // Draw
+        result = 'draw'; // Draw
         return(result);
     } else {
-        result = "C"; // CPU Wins
+        result = 'cpuWins'; // CPU Wins
         return(result);
     }
 }
 
-
-// Capitalises the first char of a string, set rest to lowercase & returns result
-function capitaliseFirst(word) { 
-    let firstLetter = word.charAt(0);
-    let restOfWord = word.slice(1);
-    return(firstLetter.toUpperCase() + restOfWord.toLowerCase());
-  }
+let playerWins = 0;
+let computerWins = 0;
+let numberOfDraws = 0;
 
 
 
 
 function game() {
-    let playerWins = 0;
-    let computerWins = 0;
-    let numberOfDraws = 0;
-    let result = "";
-    let computerSelection = "";
-    let playerSelection = "";
-    let playerInput = "";
+    let result = '';
+    let computerSelection = '';
+    let playerSelection = '';
 
-    for (let i = 0; i < 5; i++) {
+    const selection = document.querySelector('.selection');
 
-        playerInput = capitaliseFirst(prompt("Enter your choice: "));
-        result = playRound(playerSelection = playerInput, computerSelection = computerPlay());
+    const rock = document.querySelector('#rock');
+    rock.addEventListener('click', () => {
+        result = playRound(playerSelection = 'rock', computerSelection = computerPlay());
 
-        console.log("Round " + (i + 1));
-        console.log("Player: " + playerSelection);
-        console.log("CPU: " + computerSelection);
+        selection.textContent = `P1: ${playerSelection} CPU: ${computerSelection} Result: ${result}`;
 
+        tallyResult(result);
 
-        if (result == "P") {
-            playerWins++;
-            console.log("Player WINS!")
-        } else if (result == "C") {
-            computerWins++;
-            console.log("CPU WINS!")
-        } else {
-            numberOfDraws++;
-            console.log("It's a DRAW")
-        }
+    });
 
+    const paper = document.querySelector('#paper');
+    paper.addEventListener('click', () => {
+        result = playRound(playerSelection = 'paper', computerSelection = computerPlay());
 
-     }
-     console.log("Player wins: " + playerWins);
-     console.log("CPU Wins: " + computerWins);
-     console.log("Number of Draws: " + numberOfDraws);
+        selection.textContent = `P1: ${playerSelection} CPU: ${computerSelection} Result: ${result}`;
+    
+        tallyResult(result);
+
+    });
+
+    const scissors = document.querySelector('#scissors');
+    scissors.addEventListener('click', () => {
+        result = playRound(playerSelection = 'scissors', computerSelection = computerPlay());
+
+        selection.textContent = `P1: ${playerSelection} CPU: ${computerSelection} Result: ${result}`;
+   
+        tallyResult(result);
+
+    });
+ 
+}
+
+function tallyResult(result) {
+    const score = document.querySelector('.score');
+    if (result == 'playerWins') {
+        playerWins++;
+        score.textContent = `P1: ${playerWins} CPU: ${computerWins} Draws: ${numberOfDraws}`;
+    } else if (result == 'cpuWins') {
+        computerWins++;
+        score.textContent = `P1: ${playerWins} CPU: ${computerWins} Draws: ${numberOfDraws}`;
+    } else {
+        numberOfDraws++;
+        score.textContent = `P1: ${playerWins} CPU: ${computerWins} Draws: ${numberOfDraws}`;
+    }
 }
 
 game();
